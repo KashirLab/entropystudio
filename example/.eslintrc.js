866d2faa506d2edc1d238a2c2fe80df548d1a739
Darrell <zh0521@gmail.com>
2026-09-16 11:30:45 +0900

    feat: add ESLint overrides for JavaScript and JSX files with Babel configuration

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,4 +1,16 @@
+const path = require('node:path');
+
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      parserOptions: {
+        babelOptions: {
+          configFile: path.join(__dirname, 'babel.config.js'),
+        },
+      },
+    },
+  ],
 };
